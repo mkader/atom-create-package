e21311544e87b75cac6bdc7cf1b0760300f4b44d
mohideen.kader <mohideen.kader@TXGDC-KMOHIDEEN>
2020-03-27 11:28:49 -0500

    new test package created

---
+++ spec/mak-test-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MakTestPackageView from '../lib/mak-test-package-view';
+
+describe('MakTestPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
